364192be336ab0ed0b1a66430c0acd356f094fdd
Tigor <tigor.hutasuhut@gmail.com>
2019-08-29 21:43:09 +0700

    create purl link page and navbar icon fix

--- controllers/UrlController.js
+++ controllers/UrlController.js
@@ -1,7 +1,18 @@
 const { Url } = require('../models');
 
 class UrlController {
+    static showCreate(req, res) {
+        res.render('app/template', { page: "create", err: req.query.err, session: req.session })
+    }
 
+    static create(req, res) {
+        Url.create(req.body)
+            .then((result) => {
+                res.redirect('./')
+            }).catch((err) => {
+                res.redirect(`./create?err=${err.message}`)
+            });
+    }
 }
 
 module.exports = UrlController
